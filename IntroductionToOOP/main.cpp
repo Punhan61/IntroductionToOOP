f83ca6a38d9bb3119b511b8a434ef9340a5ec1dd
mak-p <mak-p@DESKTOP-PG1IP70>
2022-10-29 17:48:59 +0300

    Fraction

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include<iostream>
 using namespace std;
-
+using std::cin;
+using std::cout;
+using std::endl;
 #define tab "\t"
 #define delimiter "\n------------------------------------------------------------------\n"
 
@@ -26,7 +28,7 @@ public:
 		this->y = y;
 	}
 
-	//       Constructors
+	//             Constructors
 
 	/*Point()
 	{
@@ -70,7 +72,30 @@ public:
 		return *this;
 	}
 
-	      /// МЕТОДЫ 
+	Point& operator++()  //Prefix Increment
+	{
+		x++;
+		y++;
+		return *this;
+	}
+
+	Point operator++(int/* здесь возможен только int */)
+	{
+		Point old = *this;
+		x++;
+		y++;
+		return old;
+	}
+
+	Point& operator()(double x, double y)
+	{
+		set_x(x);
+		set_y(y);
+		return *this;
+	}
+
+	//               МЕТОДЫ 
+
 	double distance(const Point& other)const
 	{
 		/*double x_distance = this->x - other.x;
@@ -87,18 +112,59 @@ public:
 };
 
    double distance(const Point& A,const Point& B)
-{
+   {
 	/*double x_distance = A.get_x() - B.get_x();
 	double y_distance = A.get_y() - B.get_y();
 	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
 	return distance;*/
 	return sqrt(pow(A.get_x() - B.get_x(), 2) + pow(A.get_y() - B.get_y(), 2));
-}
+   }
+
+   Point operator+(const Point& left, const Point& right)
+   {
+	   Point res;
+	   res.set_x(left.get_x() + right.get_x());
+	   res.set_y(left.get_y() + right.get_y());
+	   return res;
+   }
+   bool operator==(const Point& left, const Point& right)
+   {
+	   return left.get_x() == right.get_x() && left.get_y() == right.get_y();
+	  /* if (left.get_x() == right.get_x() && left.get_y() == right.get_y())
+		   return true;
+	   else
+		   return false;*/
+   }
+
+   bool operator!=(const Point& left, const Point& right)
+   {
+	   return !(left == right);
+   }
+
+   bool operator<(const Point& left, const Point& right)
+   {
+	   return left.get_x() < right.get_x() && left.get_y() < right.get_y();
+   }
+   
+   bool operator<=(const Point& left, const Point& right)
+   {
+	   return left.get_x() <= right.get_x() && left.get_y() <= right.get_y();
+   }
+
+   ostream& operator<<(ostream& os, const Point& obj)
+   {
+	   os << "X = " << obj.get_x();
+	   os << "\tY = " << obj.get_y();
+	   return os;
+   }
 
 //#define STRUCT_POINT
 //#define GET_SET_CHECK
 //#define DISTANCE_CHEK 
 //#define CONSTRUCTOR_CHEK
+//#define ASSIGNMENT_OPERATOR_CHECK
+//#define ARIFNMETICAL_OPERATOR_CHECK
+//#define COMPERESON_OPERATORS 
 
 void main()
 {
@@ -162,9 +228,40 @@ void main()
 	E.print();
 #endif // CONSTRUCTOR_CHEK
 
+#ifdef ASSIGNMENT_OPERATOR_CHECK
 	Point A, B, C;
 	cout << delimiter << endl;
 	A = B = C = Point(2, 3);
 	cout << delimiter << endl;
+#endif // ASSIGNMENT_OPERATOR_CHECK
 
+#ifdef ARIFNMETICAL_OPERATOR_CHECK
+	Point A(2, 3);
+	Point B(7, 8);
+	Point C = A + B;
+	A.print();
+	B.print();
+	C.print();
+	Point D = C++;
+	C.print();
+	D.print();
+
+	for (Point i; i.get_x() < 10; ++i)	i.print();
+#endif // ARIFNMETICAL_OPERATOR_CHECK
+
+#ifdef COMPERESON_OPERATORS
+	cout << (Point(2, 3) != Point(7, 8)) << endl;
+	for (Point i; i < Point(10, 10); ++i)
+	{
+		i.print();
+	}
+#endif // COMPERESON_OPERATORS
+
+	Point A;
+	//A.print();
+	/*A.set_x(2);
+	A.set_y(3);*/
+	A(2, 3);
+	//A.print();
+	cout << A << endl;
 }
